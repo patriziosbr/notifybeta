5ec2398b189e7412cdb5b17e70373fb5c3682992
patriziosbr <patrizio.sbrozzi@gmail.com>
2023-08-29 14:39:17 +0200

    da capo

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const BASE_URL_BASE = process.env.API_URL_ENV;
 
-const API_URL = BASE_URL_BASE + "/api/users/";
+const API_URL = "/api/users/";
 // const API_URL = '/api/users/'
 
 // Register user
